2b1b2378da94806149ee59371e63a6b9e799525c
sarav <mkmohankumar261998@gmail.com>
2024-07-12 20:39:50 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,74 +1,61 @@
 import { useEffect, useState } from "react";
 
-export default function App() {
-  const [countries, setCountries] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+const CountryCard = ({ name, flagImg, flagAltTxt }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        padding: "10px",
+        margin: "10px",
+        border: "1px solid black",
+        borderRadius: "8px",
+        width: "200px",
+        height: "200px",
+      }}
+    >
+      <img
+        src={flagImg}
+        alt={flagAltTxt}
+        style={{ width: "100px", height: "100px" }}
+      />
+      <h2>{name}</h2>
+    </div>
+  );
+};
 
+function Countries() {
+  const API_URL = "https://restcountries.com/v3.1/all";
+  const [countries, setCountries] = useState([]);
+  console.log({ countries });
   useEffect(() => {
-    fetch("https://xcountries-backend.azurewebsites.net/all")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(`HTTP error! status: ${res.status}`);
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setCountries(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data: ", error.message);
-        setError(error.message);
-        setLoading(false);
-      });
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error fetching data: ", error.message));
   }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  const cardStyle = {
-    width: "200px",
-    border: "1px solid #ccc",
-    borderRadius: "10px",
-    margin: "10px",
-    padding: "10px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  };
-
-  const imageStyle = {
-    width: "100px",
-    height: "100px",
-  };
-
-  const containerStyle = {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    alignItems: "center", // Corrected typo here
-    height: "100vh",
-  };
-
   return (
-    <div style={containerStyle}>
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
       {countries.map((country) => (
-        <div key={country.cca3} style={cardStyle}>
-          <img
-            src={country.flags.png}
-            alt={`Flag of ${country.name.common}`}
-            style={imageStyle}
-          />
-          <h2>{country.name.common}</h2>
-        </div>
+        <CountryCard
+          name={country.name.common}
+          flagImg={country.flags.png}
+          flagAltTxt={country.flags.alt}
+        />
       ))}
     </div>
   );
 }
+
+export default Countries;
